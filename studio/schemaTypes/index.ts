ef967b82891fcd13cd5d93a148b2e49a490dade3
coakleycts-design <coakley.cts@gmail.com>
2026-09-08 14:01:50 -0500

    Register all VNH page schemas

--- studio/schemaTypes/index.ts
+++ studio/schemaTypes/index.ts
@@ -1,4 +1,7 @@
+import {contactPage} from './contactPage'
 import {homePage} from './homePage'
+import {missionPage} from './missionPage'
+import {shopPage} from './shopPage'
 import {siteSettings} from './siteSettings'
 
-export const schemaTypes = [siteSettings, homePage]
+export const schemaTypes = [siteSettings, homePage, missionPage, contactPage, shopPage]
