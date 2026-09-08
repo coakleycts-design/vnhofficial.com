3e2e849ae27af42973c931f257078d42a45869f8
coakleycts-design <coakley.cts@gmail.com>
2026-09-08 11:01:23 -0500

    Add canonical XML sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,26 @@
+export const prerender = true;
+
+const SITE = 'https://vnhofficial.com';
+const routes = ['/', '/shop', '/mission', '/contact'];
+
+const escapeXml = (value: string) => value
+  .replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&apos;');
+
+export function GET() {
+  const urls = routes
+    .map((route) => `  <url><loc>${escapeXml(new URL(route, SITE).toString())}</loc></url>`)
+    .join('\n');
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`;
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=3600'
+    }
+  });
+}
