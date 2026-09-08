830896b42e91a70bb9c751cc325367a2c6d5ea2d
coakleycts-design <coakley.cts@gmail.com>
2026-09-08 11:29:09 -0500

    Configure VNH Sanity project

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -3,10 +3,8 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'iec4sn1b'
 const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
-
-if (!projectId) throw new Error('SANITY_STUDIO_PROJECT_ID is required')
 
 const singletonTypes = new Set(['siteSettings', 'homePage'])
 
